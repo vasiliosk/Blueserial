fb262fd8c6c523359458829113d7c539f76e326d
vasiliosk <vkk@vkintsys.com>
2017-10-24 12:05:36 -0700

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,36 @@
+################################################################################
+#
+#	install.sh
+#	
+#	This script is used for installing the blueserial.sh script.
+#
+################################################################################
+#!/bin/sh
+
+#	Script to be copied.
+scriptname="blueserial.sh"
+#	Name of the program. This is what will be used from the terminal to run it.
+PROG_NAME="blueserial"
+#	Directory from which the script wil execute. If you would like it to run
+#	from a different directory, just change the path below.
+execut_dir="/usr/sbin"
+#	Get the name of the user running this script.
+USR_NAME=$(echo $USER)
+
+case ${USR_NAME} in
+	root) break ;;
+	*)	echo ""
+		echo  " ${USR_NAME}:  Please execute the script using 'sudo'" 
+		echo ""
+		echo " example: 'sudo '"
+		exit 0	;;
+	esac	#	end switch
+	
+sh -c "cp ${scriptname} ${execut_dir}/${PROG_NAME}"
+
+ls -l ${execut_dir}/${PROG_NAME}
+
+echo "Done."
+
+exit 0
+################################################################################
